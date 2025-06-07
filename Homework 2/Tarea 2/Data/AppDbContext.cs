c7845794ac733a5843fccba5142c6554297f6c49
o.pereyra <onasis.pereyra@gmail.com>
2025-06-06 20:31:39 -0400

    Update
    Creating the project, the Student class and connecting to a data base.

---
+++ Homework 2/Tarea 2/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tarea_2.Models.Entities;
+
+
+
+namespace Tarea_2.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ Homework 2/Tarea 2/Models/Entities/Student.cs
@@ -0,0 +1,14 @@
+﻿namespace Tarea_2.Models.Entities
+{
+    public class Student
+    {
+        public Guid Id { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Career { get; set; }
+
+    }
+}
