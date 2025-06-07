6b5e66bfdd31417d962a9f053a7320de00f950a7
o.pereyra <onasis.pereyra@gmail.com>
2025-06-06 22:39:45 -0400

    Update
    Making the reading part of the crud and starting with the update part.

---
+++ Homework 2/CRUD_API/Controllers/StudentsController.cs
@@ -0,0 +1,35 @@
+﻿using CRUD_API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly AppDbContext dbContext;
+
+        public StudentsController(AppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            var allStudents = dbContext.Students.ToList();
+
+            return Ok(allStudents);
+        }
+
+        [HttpPost]
+
+        public IActionResult AddStudents()
+        {
+
+        }
+    }
+
+    
+}
--- Homework 2/CRUD_API/Models/Entitie/Student.cs
+++ Homework 2/CRUD_API/Models/Entitie/Student.cs
@@ -2,7 +2,7 @@
 {
     public class Student
     {
-        public Guid Id { get; set; }
+        public int Id { get; set; }
         public required string FirstName { get; set; }
         public required string LastName { get; set; }
         public string Address { get; set; }
