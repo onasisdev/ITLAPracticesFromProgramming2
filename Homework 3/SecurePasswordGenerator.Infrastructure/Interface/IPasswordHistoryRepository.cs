9773c74d11a261ecd27c0d8d43763541ce770b73
o.pereyra <onasis.pereyra@gmail.com>
2025-07-17 16:33:57 -0400

    Update
    Making the repository interface.

---
+++ Homework 3/SecurePasswordGenerator.Infrastructure/Interface/IPasswordHistoryRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SecurePasswordGenerator.Domain;
+
+using SecurePasswordGenerator.Domain.Entities;
+
+namespace SecurePasswrodGenerator.Infraestructure.Interfaces
+{
+    public interface IPasswordHistoryRepository
+    {
+        Task<IEnumerable<PasswordHistory>> GetAllAsync();
+        Task<PasswordHistory> GetByIdAsync(int id);
+        Task AddAsync(PasswordHistory history);
+        Task UpdateAsync(PasswordHistory history);
+        Task DeleteAsync(int id);
+    }
+}
